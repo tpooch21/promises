e4f46473c309f8601ae8c1d231a35a7152ad4c08
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-14 10:48:31 -0700

    Define pluckFirstLineFromFile in Node style callback pattern

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,21 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  // use readFile to read contents of file and return data
+  // split data at slashes or newline characters
+  // return first item in newly split data
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      callback(err);
+    } else {
+      console.log('logging fileData returned from readFile => ', fileData.toString());
+      var firstLine = fileData.toString().split(/\n/)[0];
+
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
